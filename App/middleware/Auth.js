a37f16b07d471e6d6d246fb8a64152544dd2416b
karthikjohnn <karthickyamkk2001@gmail.com>
2025-01-03 16:32:53 +0530

    Bearer token for IOS

--- App/middleware/Auth.js
+++ App/middleware/Auth.js
@@ -3,10 +3,11 @@ const jwt = require("jsonwebtoken");
 const jwtSecret = process.env.JWT_SECRET;
 
 const isAuth = (req, res, next) => {
-  console.log(req);
-  const token = req.cookies.token;
+  console.log("Cookies",req.cookies);
+  console.log("Headers",req.headers);
+  const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
   if (!token) {
-    return res.status(401).json({ message: "No token, authorization denied" });
+    return res.status(401).json({ message: "Authorization denied!" });
   }
   jwt.verify(token, jwtSecret, (err, user) => {
     if (err) {
